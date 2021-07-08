0379263812cb239a65fe256d77d1be02c8c3b9ac
manish591 <manishdevrani777@gmail.com>
2021-07-08 10:56:10 +0530

    style- app styled

--- lucky.js
+++ lucky.js
@@ -11,11 +11,22 @@ function showLuckyNumber () {
         sum += parseInt(item);
     })
 
+    if(luckyNumber.value === '' || dateOfBirth.value === '') {
+        return output.innerHTML = 'Please fill these input first.'
+    } else if (dateOfBirth.value.length < 8 || dateOfBirth.value.length > 8) {
+        return output.innerHTML = 'Write date of birth properly.'
+    } else if (luckyNumber.value.length > 1 || luckyNumber.value === 0) {
+        return output.innerHTML = 'Read the note properly!'
+    }
+
     if (sum % parseInt(luckyNumber.value) === 0) {
         output.innerHTML = 'This is your lucky number!'
     } else {
         output.innerHTML = 'No!'
     }
+
+    dateOfBirth.value = '';
+    luckyNumber.value = '';
 }
 
 showButton.addEventListener('click', showLuckyNumber);
