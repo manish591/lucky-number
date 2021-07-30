de2c547630e46445ad0e6f252563c392872b47ac
manish591 <manishdevrani777@gmail.com>
2021-07-30 21:09:06 +0530

    fix: avoid user to enter negative value

--- lucky.js
+++ lucky.js
@@ -8,6 +8,7 @@ const ouputGif1 = document.querySelector('.outputgif1');
 const ouputGif2 = document.querySelector('.outputgif2');
 
 function showLuckyNumber () {
+    resetGif();
     let string = dateOfBirth.value;
     let arrayDateOfBirth = string.split("-");
     let sum = 0;
@@ -18,9 +19,10 @@ function showLuckyNumber () {
 
     if(luckyNumber.value === '' || dateOfBirth.value === '') {
         return output.innerHTML = 'Please fill these input first.'
-    } else if (luckyNumber.value === 0) {
-        return output.innerHTML = 'Please Don\'t enter 0 as a lucky number!'
+    } else if (Number(luckyNumber.value) === 0 || Math.sign(Number(luckyNumber.value)) === -1) {
+        return output.innerHTML = 'Please Don\'t enter 0 and negative as a lucky number!';
     }
+
 
     if (sum % parseInt(luckyNumber.value) === 0) {
         output.innerHTML = 'This is your lucky number!';
@@ -29,7 +31,6 @@ function showLuckyNumber () {
         output.innerHTML = 'No! This is not your lucky number.'
         ouputGif2.classList.remove('hide');
     }
-
     dateOfBirth.value = '';
     luckyNumber.value = '';
 }
@@ -39,3 +40,8 @@ showButton.addEventListener('click', showLuckyNumber);
 hidePrivacyNotice.addEventListener('click', () => {
     privacySection.style.display = 'none';
 })
+
+function resetGif() {
+    ouputGif1.classList.add('hide');
+    ouputGif2.classList.add('hide');
+}
